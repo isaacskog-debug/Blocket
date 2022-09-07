9ce39fd070f40de6f25bb02f22a0a7f9f949cddd
isaacskog-debug <111750854+isaacskog-debug@users.noreply.github.com>
2022-09-07 09:54:45 +0200

    Uppdatera projekt

--- src/main/java/com/example/securitystart/SecurityApplication.java
+++ src/main/java/com/example/securitystart/SecurityApplication.java
@@ -1,12 +1,13 @@
-package com.example.demo;
+package com.example.securitystart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SecurityApplication.class, args);
 	}
+
 }
--- src/test/java/com/example/securitystart/SecurityStartApplicationTests.java
+++ src/test/java/com/example/securitystart/SecurityStartApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.securitystart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SecurityStartApplicationTests {
 
 	@Test
 	void contextLoads() {
