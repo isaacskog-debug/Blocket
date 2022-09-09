0d375d1a3931fe331704257970c2ea54aefc9ee9
Tobias Traff <tobiastraff@live.se>
2022-09-09 13:12:27 +0200

    test

--- src/test/java/com/example/securitystart/SecurityStartApplicationTests.java
+++ src/test/java/com/example/securitystart/SecurityStartApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.securitystart;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,10 @@ class SecurityStartApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	public void addNewAdvert() {
+		//AdvertRepository repository=new AdvertRepository();
+		Advert advert=new Advert(1L,"Space Station ISS",1,"Is in low orbit, no shiping","Z","USD");
+		Assertions.assertEquals("Space Station ISS", advert.getName());
+	}
 }
